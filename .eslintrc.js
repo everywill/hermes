f9dc282cb7d24ea7aecc3892309b88dacf6f313c
everywill <everywillatchina@gmail.com>
2018-05-08 14:31:48 +0800

    modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  root: true,
-  extends: ["kaola/esnext"],
-  parser: "babel-eslint",
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  // add your custom rules here
-  'rules': {
-    'no-unused-vars': ["error", { "ignoreRestSiblings": true }]
-  }
-}
+    root: true,
+    extends: ['kaola/esnext'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    env: {
+        browser: true
+    },
+    // add your custom rules here
+    rules: {
+        'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+        'max-statements': 'off',
+        'no-use-before-define': 'off'
+    }
+};
