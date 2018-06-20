059b1e32fa91dd1bf044900d744e5b0b8647a54f
everywill <everywillatchina@gmail.com>
2018-06-20 22:14:49 +0800

    ciphertext

--- vendor/aes/aes.js
+++ vendor/aes/aes.js
@@ -2,7 +2,7 @@ const AES = require('crypto-js/aes');
 const DEFAULT_KEY = '7a82fb3e5489cfffc42be3f0b9f9b2ac';
 
 function encrypt(message) {
-    return AES.encrypt(message, DEFAULT_KEY);
+    return AES.encrypt(message, DEFAULT_KEY).ciphertext;
 }
 
 module.exports = encrypt;
