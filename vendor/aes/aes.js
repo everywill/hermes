9e8cf404ab780573c54bc5dc2e30cec4a7816661
everywill <everywillatchina@gmail.com>
2018-06-22 15:13:10 +0800

    modify aes

--- vendor/aes/aes.js
+++ vendor/aes/aes.js
@@ -1,8 +1,16 @@
-const AES = require('crypto-js/aes');
-const DEFAULT_KEY = '7a82fb3e5489cfffc42be3f0b9f9b2ac';
+const CryptoJS = require('crypto-js');
+// AES KEY
+const key = CryptoJS.enc.Hex.parse('7a82fb3e5489cfffc42be3f0b9f9b2ac');
+const DEFAULT_KEY = CryptoJS.enc.Hex.parse('7a82fb3e5489cfffc42be3f0b9f9b2ac');
 
 function encrypt(message) {
-    return AES.encrypt(message, DEFAULT_KEY).ciphertext;
+    return CryptoJS.AES.encrypt(message, DEFAULT_KEY, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7,
+        iv: null
+    })
+        .ciphertext.toString()
+        .toUpperCase();
 }
 
 module.exports = encrypt;
