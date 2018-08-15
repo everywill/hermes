1dde787be7410e7d904b637920088d89757ab993
everywill <everywillatchina@gmail.com>
2018-08-15 13:00:50 +0800

    console

--- scripts/cdn.js
+++ scripts/cdn.js
@@ -5,23 +5,31 @@ require('dotenv').config();
 
 const client = new NosClient();
 
-client.setAccessId(process.env.ACCESS_KEY);
-client.setSecretKey(process.env.SECRET_KEY);
-client.setEndpoint(process.env.END_POINT);
+const ACCESS_KEY = process.env.ACCESS_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+const END_POINT = process.env.END_POINT;
+const BUCKET = process.env.BUCKET;
+
+client.setAccessId(ACCESS_KEY);
+client.setSecretKey(SECRET_KEY);
+client.setEndpoint(END_POINT);
+
+const filename = `hermes.min.${pkg.version}.js`;
+
+console.log('Start uploading SDK...');
 
 client.put_file(
     {
-        bucket: process.env.BUCKET,
-        key: `hermes.min.${pkg.version}.js`,
+        bucket: BUCKET,
+        key: filename,
         filepath: path.join(__dirname, '../dist', 'hermes.min.js')
     },
-    (err, result) => {
-        if (err) {
+    (result) => {
+        if (result.statusCode === 200) {
+            console.log('Uploading succeed!');
+            console.log(`check it on https://${BUCKET}.${END_POINT}/${filename}`);
+        } else {
             console.log('SDK upload failed!');
-            console.error(err);
-            return;
         }
-        console.log('SDK upload succeed!');
-        console.log(result);
     }
 );
